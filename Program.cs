ef350f2179f9a19b5877c066a0f9bb3bf2ced68d
Dra.Telma Sá <matheusps1407@gmail.com>
2021-01-07 23:15:56 -0300

    Projeto de Console Finalizado

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Projeto_de_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome");
+            string nomecompleto = Console.ReadLine();
+            Console.WriteLine("Seja Bem-vindo(a), "+nomecompleto);
+
+            //programa exercicio - aula
+
+            int valorUm;
+            int valorDois;
+        
+
+            //Entrada de Dados
+            Console.WriteLine("Some dois valores inteiros");
+            Console.WriteLine("--------------------------");
+            Console.Write("Digite o primeiro valor: ");
+            valorUm = int.Parse (Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            valorDois = int.Parse (Console.ReadLine());
+            Console.WriteLine("--------------------------");
+
+            //Processamento
+            int soma = valorUm + valorDois;
+
+            //Exibição
+            Console.WriteLine("O resultado final da soma de "+valorUm+" e "+valorDois+" é igual à: "+soma);
+            
+        }
+    }
+}
